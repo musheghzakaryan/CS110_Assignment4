e17b1e4d4b04151fee7ad5558fe8234a543a475d
Mushegh <mzakaryan2002@gmail.com>
2020-11-09 14:39:27 +0400

    first commit

---
+++ utils.js
@@ -0,0 +1,86 @@
+function matrixProduct(mat1, mat2) {
+    let result = [];
+    for (let i = 0; i < mat1.length; i++) {
+        result[i] = [];
+        for(let j = 0; j < mat2.length; j++) {
+            let sum = 0;
+            for(let k = 0; k < mat1.length; k++) {
+                sum += mat1[i][k] * mat2[k][j];
+            }
+            result[i][j] = sum;
+        }
+    }
+    return result;
+
+    
+
+}
+
+function beautyLog(matrixProduct){
+    for(let i = 0; i < matrixProduct.length; i++){
+        console.log(matrixProduct[i]);
+    }
+}
+
+
+
+let mat1 = [
+    [1, 2],
+    [3, 4]
+
+]
+
+let mat2 = [
+    [5, 6],
+    [7, 8]
+]
+
+beautyLog(matrixProduct(mat1, mat2));
+
+// 1st
+
+
+
+
+function sumOfMatrcies(A, B){
+    if(A.length !== B.length || A[0].length !== B[0].length){
+        console.log("error");
+        return;
+    }
+    for(let row = 0; row < A.length; row++){
+    for(let col = 0; col < A[0].length; col++){
+        A[row][col] += B[row][col]
+
+    }
+}
+return A;
+
+}
+
+function beautyLog(sumOfMatrcies){
+    for(let i = 0; i < sumOfMatrcies.length; i++){
+        console.log(sumOfMatrcies[i]);
+    }
+}
+
+
+
+let A = [
+    [1, 2],
+    [3, 4]
+]
+
+let B = [
+    [5, 6],
+    [7, 8]
+]
+
+beautyLog(sumOfMatrcies(A, B));
+
+// 2st
+
+module.exports = {
+    beautyLog,
+    sumOfMatrcies,
+    matrixProduct
+}
